9ec8d01564ba24ba6f199de33be9a9d1f37e7aa2
Wonkers <e_duckie@hotmail.com>
2021-08-08 13:01:38 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-#include "support/gcc8_c_support.h"
-#include <proto/exec.h>
-#include <proto/dos.h>
-#include <proto/graphics.h>
-#include <graphics/gfxbase.h>
-#include <graphics/view.h>
-#include <exec/execbase.h>
-#include <graphics/gfxmacros.h>
-#include <hardware/custom.h>
-#include <hardware/dmabits.h>
-#include <hardware/intbits.h>
 #include "Game.h"
 
 struct ExecBase *SysBase;
@@ -18,11 +7,10 @@ int main()
 {
 	SysBase = *((struct ExecBase**)4UL);
 
-	KPrintF("Here before Game\n");
 	Game game;
 
 	game.Run();
-	KPrintF("Here After Game\n");
+
 	return 0;
 }
 
